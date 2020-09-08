018c06fd986927104abd3438babfb021f20e6368
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-09-07 21:51:38 -0300

    GoBarber

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,39 @@
+import { startOfHour } from "date-fns";
+
+import Appointment from "../models/Appointments";
+import AppointmentsRepository from "../repositories/AppointmentsRepository";
+import { fromString } from "uuidv4";
+
+interface RequestAppointmentService {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: RequestAppointmentService): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error("This appoinment is already booked!");
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
